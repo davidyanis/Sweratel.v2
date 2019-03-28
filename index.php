2e30c48f7dbcaa8598dfe4189cf234a20d325a01
Peter Cavar <33002520+PCavar@users.noreply.github.com>
2019-03-28 16:17:28 +0100

    push

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./semantic/semantic.css">
+    <script src="./semantic/semantic.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    
+<div class="wrapper-for-site">
+    <div class="container-for-first-picture"> 
+
+    </div>
+</div>
+    
+</body>
+</html>
