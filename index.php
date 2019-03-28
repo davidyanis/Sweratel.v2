cdf1fd3ce27209d4ae74fd079174fbb8b98583fd
Peter Cavar <33002520+PCavar@users.noreply.github.com>
2019-03-28 16:36:14 +0100

    push

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="./semantic/semantic.css">
     <script src="./semantic/semantic.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
     <title>Document</title>
 </head>
 
